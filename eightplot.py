fcced6346eeac8c458b68c8949b648a8aa1d4e02
Lorenzo Bivens <lorenzo.bivens@cbre.com>
2017-04-27 11:23:42 -0700

    I am not sure what I am doing

--- eightplot.py
+++ eightplot.py
@@ -10,39 +10,39 @@ NORMAL = chr(27) + '[0m'
 SUPERSCRIPT = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹']
 SUBSCRIPT = ['₀', '₁', '₂', '₃', '₄', '₅', '₆', '₇', '₈', '₉']
 
-class EightPlot:
-    def __init__(self):
-        pass
+def sparkline(label, data):
+    """
+    Plots sparkline chart given a list of float data
+    """
+    max_data = max(data)
+    min_data = min(data)
 
-    def sparkline(self, label, data):
-        """
-        Plots sparkline chart given a list of float data
-        """
-        max_data = max(data)
-        min_data = min(data)
+    max_label = ''.join([SUPERSCRIPT[int(digit)] for digit in str(int(max_data))])
+    min_label = ''.join([SUBSCRIPT[int(digit)] for digit in str(int(min_data))])
 
-        max_label = ''.join([SUPERSCRIPT[int(digit)] for digit in str(int(max_data))])
-        min_label = ''.join([SUBSCRIPT[int(digit)] for digit in str(int(min_data))])
+    interval_width = ceil((max_data-min_data) / 7.0) if max_data > min_data\
+            else ceil(max_data / 7.0)
 
-        interval_width = ceil((max_data-min_data) / 7.0) if max_data > min_data\
-                else ceil(max_data / 7.0)
+    output = ''.join([TICKS[int(round((each-min_data) / interval_width))]
+                     if round(each) != 0 else TICKS[0] for each in data])
 
-        output = ''.join([TICKS[int(round((each-min_data) / interval_width))]
-                         if round(each) != 0 else TICKS[0] for each in data])
-
-        print '%7.7s %70s\t %6s%-7s' % (label, output, min_label, max_label)
-        
-    def heatline(self, label, data):
-        """
-        Plots heatline chart given a list of float data
-        """
-        max_data = max(data)
-        min_data = min(data)
+    print '%7.7s %70s\t %6s%-7s' % (label, output, min_label, max_label)
     
-        interval_width = ceil((max_data-min_data) / 22.0) if max_data > min_data\
-                else ceil(max_data / 22.0)
-    
-        output = ''.join([GREY_HEAT[int(round((each-min_data) / interval_width))]
-                         if round(each) != 0 else GREY_HEAT[0] for each in data])
-    
-        print '%7.7s %70s%s' % (label, output, NORMAL)
+def heatline(label, data):
+    """
+    Plots heatline chart given a list of float data
+    """
+    max_data = max(data)
+    min_data = min(data)
+
+    interval_width = ceil((max_data-min_data) / 22.0) if max_data > min_data\
+            else ceil(max_data / 22.0)
+
+    output = ''.join([GREY_HEAT[int(round((each-min_data) / interval_width))]
+                     if round(each) != 0 else GREY_HEAT[0] for each in data])
+
+    print '%7.7s %70s%s' % (label, output, NORMAL)
+
+
+if __name__ == '__main__':
+    sparkline("test", [1,2,3,4,5,6,7,8,6,5,4,7,8,9,9,9,7,5,4,3,2,3,7,1,2,3,4,5,6,7,8,6,5,4,7,8,9,9,9,7,5,4,3,2,3,7])
